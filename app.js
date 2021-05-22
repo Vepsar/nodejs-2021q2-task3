f24221b344730253c4a7a40810b0c9831ac34947
vepsar <vepsarvz@gmail.com>
2021-05-22 18:26:55 +0300

    fix: fix all 5 compilation problems

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
-// var db = require('./db');
+var db = require("./db");
 var user = require("./controllers/usercontroller");
 var game = require("./controllers/gamecontroller");
 
-// db.sync();
+db.sync();
 app.use(require("body-parser"));
 app.use("/api/auth", user);
 app.use(require("./middleware/validate-session"));
